426905a07a78e2ebf801bf7a15cac4d8fd3d4d46
hashinisenaratne <hashz1990@gmail.com>
2014-01-17 09:34:10 +0530

    Authentication using common active directory

--- Seranet.Api.Authentication/DummyPrincipalProvider.cs
+++ Seranet.Api.Authentication/DummyPrincipalProvider.cs
@@ -1,20 +1,36 @@
-using System.Security.Principal;
+﻿using System.Security.Principal;
+using System.DirectoryServices;
+﻿using System;
 
 namespace Seranet.Api.Authentication
 {
     public class DummyPrincipalProvider : IProvidePrincipal
     {
-        private const string Username = "username";
-        private const string Password = "password";
-
         public IPrincipal CreatePrincipal(string username, string password)
         {
-            if (username != Username || password != Password)
+            bool authenticated = false;
+
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry("LDAP://" + "99xtad001.seranet.local", username, password);
+                object nativeObject = entry.NativeObject;
+                authenticated = true;
+            }
+            catch (DirectoryServicesCOMException cex)
+            {
+                //not authenticated; reason why is in cex
+            }
+            catch (Exception ex)
+            {
+                //not authenticated due to some other exception [this is optional]
+            }
+
+            if (!authenticated)
             {
                 return null;
             }
 
-            var identity = new GenericIdentity(Username);
+            var identity = new GenericIdentity(username);
             IPrincipal principal = new GenericPrincipal(identity, new[] { "User" });
             return principal;
         }
