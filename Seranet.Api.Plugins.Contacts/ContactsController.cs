d5e5304fc55074258fca0fa15568f3f98de16fde
hashinisenaratne <hashz1990@gmail.com>
2014-01-27 10:47:40 +0530

    2012 compilation with some cleaning

--- Seranet.Api.Plugins.Contacts/ContactsController.cs
+++ Seranet.Api.Plugins.Contacts/ContactsController.cs
@@ -10,7 +10,6 @@ namespace Seranet.Api.Plugins.Contacts
 {
     public class ContactsController: ApiController
     {
-        //[SeranetAuth]
         public IEnumerable<Contact> Get()
         {
             string path = System.Environment.GetEnvironmentVariable("SERANET.API.PLUGINS.CONTACT.RESOURCEFILE");//("SERANET.API.PLUGINS.CONTACT.RESOURCEFILE");
